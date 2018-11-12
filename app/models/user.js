5282b3c4fac1714f4912aff9a75b95c23f7eafcb
sundowndev <raphael@crvx.fr>
2018-11-12 18:19:13 +0100

    Models

--- app/models/user.js
+++ app/models/user.js
@@ -1,11 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
-var UserSchema   = new Schema({
-    firstname: String,
-    lastname: String,
-    username: String,
-    password: String
+var UserSchema = new Schema({
+  firstname: {
+      type: String,
+      required: true
+  },
+  lastname: {
+      type: String,
+      required: true
+  },
+  username: {
+      type: String,
+      unique: true,
+      required: true
+  },
+  password: {
+      type: String,
+      required: true
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
