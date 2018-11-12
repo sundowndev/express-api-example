1b7da6d9b2404d664169aa142acb334cdbac87a7
sundowndev <raphael@crvx.fr>
2018-11-12 18:19:58 +0100

    404 error middleware

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 8080; // set our port
 
 app.use('/', routes);
 
+app.use(function(req, res) {
+    res.status(404).json({ message: 'Resource not found.' });
+});
+
 // Start the server
 app.listen(port);
 console.log('Server started on port ' + port);
