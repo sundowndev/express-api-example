0b1361263ea363227cf8b6b5f3055d19b7f1e0ec
sundowndev <raphael@crvx.fr>
2018-11-14 18:16:33 +0100

    Error handling middleware

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 8080; // set our port
 
 app.use('/', routes);
 
+app.use((err, req, res, next) => {
+  res.status(err.status || 400).json({ success: false, message: err.message || 'An error occured.', errors: err.error || [] });
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Resource not found.' });
 });
