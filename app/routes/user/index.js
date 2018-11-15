8201236c7de634fcd68105155b5e18006cdc64aa
sundowndev <raphael@crvx.fr>
2018-11-15 19:28:33 +0100

    User routes

--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -3,34 +3,40 @@ const user = require('express').Router();
 const profile = require('./profile');
 const update = require('./update');
 const remove = require('./delete');
-// const notes = require('./notes');
+const notes = require('./notes');
+
+const Authentication = require.main.require('./app/validation/auth');
 
 /**
- * @api {get} /user/me Get user information
+ * @api {get} /user/me Get account information
  * @apiName GetUser
  * @apiGroup User
  *
  * @apiSuccess {String} firstname Firstname of the User.
  * @apiSuccess {String} lastname  Lastname of the User.
- * @apiSuccess {String} username  Username of the User.
+ * @apiSuccess {String} email  Email of the User.
  */
-user.get('/me', profile);
+user.get('/me', Authentication, profile);
 
 /**
   * @api {put} /user/me Update account information
   * @apiName UpdateUser
   * @apiGroup User
   *
+  * @apiParam {String} Firstname new firstname.
+  * @apiParam {String} Lastname new lastname.
+  * @apiParam {String} Email new email address.
+  *
   * @apiSuccess {Object} user User object.
   */
-user.put('/me', update);
+user.put('/me', Authentication, update);
 
 /**
  * @api {delete} /user/me Delete account
  * @apiName DeleteUser
  * @apiGroup User
  */
-user.delete('/me', remove);
+user.delete('/me', Authentication, remove);
 
 /**
   * @api {get} /user/me/notes Get all notes
@@ -39,6 +45,6 @@ user.delete('/me', remove);
   *
   * @apiSuccess {Array} Array Notes of the user.
   */
-user.get('/me/notes', profile);
+user.get('/me/notes', Authentication, notes);
 
 module.exports = user;
