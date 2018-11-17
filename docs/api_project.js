319d1b9dbdb804b0c918c3b933c9e743b734a34b
sundowndev <raphael@crvx.fr>
2018-11-17 01:01:35 +0100

    API docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-11-16T14:21:42.357Z",
+    "time": "2018-11-17T00:00:47.768Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
